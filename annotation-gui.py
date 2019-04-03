607d75caa9d23f7bb08149f557aecea1775acfbe
IceCereal <ns_raghav@hotmail.com>
2019-04-03 18:29:41 +0530

    add imports and create writelog

--- annotation-gui.py
+++ annotation-gui.py
@@ -3,3 +3,17 @@
 	Objective: To annotate the phrases in 'Source*.csv'
 """
 print ("Annotation - GUI")
+
+import sys
+import json
+import subprocess
+
+from os import mkdir
+from os import listdir
+from os.path import isfile, join
+from datetime import datetime
+
+def writeLog(writeData : str):
+	with open("Logger.txt", 'a') as FLog:
+		FLog.write("\n"+str(datetime.datetime.now())+"\t"+writeData.upper())
+
